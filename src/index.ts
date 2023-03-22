c2acada1a23098b61608b2501100bf4fe85d8ab7
Aris Valdez <samus45@hotmail.es>
2023-03-22 11:25:22 -0400

    added routes

--- src/index.ts
+++ src/index.ts
@@ -4,29 +4,16 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.json());
+
 interface Heroe {
     id: Number,
     nombre: String,
     alte: String
 }
 
-const heroes: Heroe[] = [
-    {
-        id: 1,
-        nombre: "Bruce Wayne",
-        alte: "Batman"
-    },
-    {
-        id: 2,
-        nombre: "Damian Wayne",
-        alte: "Robin"
-    },
-    {
-        id: 3,
-        nombre: "Clark Kent",
-        alte: "SuperMan"
-    }
-]
+let _id = 0;
+const heroes: Heroe[] = [];
 
 app.get('/heroe', (req: Request, res: Response) => {
     res.json(heroes);
@@ -48,6 +35,65 @@ app.get('/heroe/:alte', (req: Request, res: Response) => {
     res.json(heroe);
 
 });
+
+app.post('/heroe', (req: Request, res: Response) => {
+
+    const { alte, nombre } = req.body;
+    const hero = heroes.find((hero) => hero.alte === alte);
+
+    if (hero) {
+        return res.status(400).json(
+            {
+                message: `The hero ${alte} already exist`
+            }
+        )
+    }
+
+    _id += 1;
+    const newHero = {
+        id: _id,
+        nombre,
+        alte
+    };
+
+    heroes.push(newHero);
+    res.status(201).json(newHero);
+});
+
+app.delete('/heroe/:alte', (req: Request, res: Response) => {
+    const { alte } = req.params;
+    const index = heroes.findIndex(
+        (hero) =>
+            hero.alte.toLowerCase() === alte.toLowerCase());
+
+    if (index < 0) {
+        return res.status(404).json(`The hero ${alte} not found`)
+    }
+
+    const hero = heroes.splice(index, 1);
+
+    res.json(hero);
+});
+
+
+app.put('/heroe/:id', (req: Request, res: Response) => {
+    const { alte, nombre } = req.body;
+    const { id } = req.params;
+
+    const hero = heroes.find((hero) => hero.id === Number.parseInt(id))
+
+    if (!hero) {
+        return res.status(401).json({
+            message: `The hero ${alte} not found`
+        })
+    }
+
+    hero.alte = alte !== undefined ? alte : hero.alte;
+    hero.nombre = nombre !== undefined ? nombre : hero.alte;
+
+    res.json(hero);
+})
+
 
 app.listen(port, () => {
     console.log(`The application is listening on port ${port}!`);
